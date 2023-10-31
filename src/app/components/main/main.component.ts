603e2122493a1c55c683826e5664ebc3dcad8354
Tugrulfg <tugrultugrul6@gmail.com>
2023-10-31 21:28:31 +0300

    Settings menu added

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faMessage ,faBell ,faUser,faPaperPlane} from '@fortawesome/free-regular-svg-icons';
-import { faPhone,faGear,faRightFromBracket ,faSearch} from '@fortawesome/free-solid-svg-icons';
+import { faPhone,faGear,faRightFromBracket ,faSearch, faArrowLeft} from '@fortawesome/free-solid-svg-icons';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { DataService } from 'src/app/shared/data.service';
 
@@ -19,6 +19,9 @@ export class MainComponent implements OnInit{
   settings = faGear;
   log_out = faRightFromBracket;
   send = faPaperPlane;
+  arrow = faArrowLeft;
+
+  showSettings = false;
 
   constructor(private firebaseService: FirebaseService,private sharedService:DataService) { }
   
@@ -30,6 +33,23 @@ export class MainComponent implements OnInit{
     return this.sharedService.getSharedData();
   }
   
+
+  openSettings(){
+    this.showSettings = !this.showSettings;
+  }
+
+  changeTheme(){
+
+  }
+
+  changeBackground(){
+
+  }
+
+  changePassword(){
+
+  }
+
   logout() {
     this.firebaseService.logout();
   }
